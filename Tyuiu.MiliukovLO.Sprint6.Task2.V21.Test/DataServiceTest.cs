dc80222bbedec758f04991c28dc053acc6ea6a35
ssadown <bibatututu@yandex.ru>
2024-11-09 17:48:01 +0500

    Task2

--- Tyuiu.MiliukovLO.Sprint6.Task2.V21.Test/DataServiceTest.cs
+++ Tyuiu.MiliukovLO.Sprint6.Task2.V21.Test/DataServiceTest.cs
@@ -1,3 +1,5 @@
+using Tyuiu.MiliukovLO.Sprint6.Task2.V21.Lib;
+
 namespace Tyuiu.MiliukovLO.Sprint6.Task2.V21.Test
 {
     [TestClass]
@@ -6,6 +8,8 @@ namespace Tyuiu.MiliukovLO.Sprint6.Task2.V21.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds = new();
+            Assert.AreEqual(15.19, ds.GetMassFunction(-5, 5)[0]);
         }
     }
 }
