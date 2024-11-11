f2f05311af643a1e8e14e2a5aaaa8532ea0a4e70
ssadown <bibatututu@yandex.ru>
2024-11-11 19:49:05 +0500

    task6

--- Tyuiu.MiliukovLO.Sprint6.Task6.V4.Test/DataServiceTest.cs
+++ Tyuiu.MiliukovLO.Sprint6.Task6.V4.Test/DataServiceTest.cs
@@ -1,3 +1,5 @@
+using Tyuiu.MiliukovLO.Sprint6.Task6.V4.Lib;
+
 namespace Tyuiu.MiliukovLO.Sprint6.Task6.V4.Test
 {
     [TestClass]
@@ -6,6 +8,10 @@ namespace Tyuiu.MiliukovLO.Sprint6.Task6.V4.Test
         [TestMethod]
         public void TestMethod1()
         {
+            string path = "C:\\Users\\bibat\\source\\repos\\Tyuiu.MiliukovLO.Sprint6\\InPutDataTask6V4.txt";
+            string wait = "SnBC nnndrM MNRjtbh RbvnKtNW UaZNpcgYjw";
+            DataService ds = new();
+            Assert.AreEqual(wait,ds.CollectTextFromFile("n", path));
         }
     }
 }
