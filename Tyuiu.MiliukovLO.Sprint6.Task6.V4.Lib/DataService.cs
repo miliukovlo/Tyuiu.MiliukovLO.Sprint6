ed077dd55756476c2f5aa4d18e628ff50f6be134
ssadown <bibatututu@yandex.ru>
2024-11-11 20:28:57 +0500

    task6

--- Tyuiu.MiliukovLO.Sprint6.Task6.V4.Lib/DataService.cs
+++ Tyuiu.MiliukovLO.Sprint6.Task6.V4.Lib/DataService.cs
@@ -4,15 +4,6 @@ namespace Tyuiu.MiliukovLO.Sprint6.Task6.V4.Lib
 {
     public class DataService : ISprint6Task6V4
     {
-        public string CollectTextFromFile(string str, string path)
-        {
-            string[] lines = File.ReadAllLines(path);
-            string text = string.Join(" ", lines);
-            string[] words = text.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
-            var result = words.Where(word => word.Contains(str, StringComparison.OrdinalIgnoreCase));
-            return string.Join(" ", result);
-        }
-
         public string CollectTextFromFile(string path)
         {
             string[] lines = File.ReadAllLines(path);
